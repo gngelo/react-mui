9b56677061fd057653c84d78710190f0ef183e19
Geraldo Angelo <gngelo@gmail.com>
2022-03-18 17:59:11 -0300

    feat: configuraçao do tema claro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes/index';
+import { LightTheme } from './shared/themes/Light';
 
 function App() {
   return (
+    <ThemeProvider theme={LightTheme}>
+
     <BrowserRouter>    
       <AppRoutes/>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
